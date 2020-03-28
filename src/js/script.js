0b20efde3839fe625be415c3afc867237920a9db
Miguel <miguel.sousa@blip.pt>
2020-03-28 23:29:10 +0000

    correction some errors

--- src/js/script.js
+++ src/js/script.js
@@ -12,3 +12,23 @@ function dropdown(question) {
     question.src = "src/svg/icon_plus.svg";
   }
 }
+
+
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
